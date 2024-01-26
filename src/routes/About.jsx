4155e514cea4f93becb952eddbb813235a1868bf
OGOZ111 <djsmallboy@gmail.com>
2024-01-26 12:19:22 +0200

    added routes, store, productSlice

---
+++ src/routes/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum illo
+      inventore magnam obcaecati consequuntur. Totam explicabo molestiae
+      praesentium, commodi ducimus tenetur aliquid libero facere, veniam eius
+      nisi. Impedit, voluptatum amet.
+    </div>
+  );
+};
+
+export default About;
---
+++ src/routes/ProductSingle.jsx
@@ -0,0 +1,9 @@
+const ProductSingle = () => {
+  return (
+    <div>
+      <h1>Product Single</h1>
+    </div>
+  );
+};
+
+export default ProductSingle;
